dae58d2b9af1cf58d758d75ab38f84ce144c0df2
Dylan Gonzalez <dylcg10@gmail.com>
2024-03-15 00:20:42 +1100

    make /out folder for executables

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-g++ -o opencv_cam Skeleton.cpp -std=c++17^
+g++ -o out/opencv_cam Skeleton.cpp -std=c++17^
     -I "C:\msys64\mingw64\include\opencv4"^
     -L "C:\msys64\mingw64\bin"^
     -lopencv_core-409 -lopencv_highgui-409 -lopencv_imgcodecs-409^
