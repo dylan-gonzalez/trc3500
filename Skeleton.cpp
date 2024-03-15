cca630196882685d61d93f6d890f259abce608b7
Matt <mlee0108@student.monash.edu>
2024-03-15 13:07:53 +1100

    crosses

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <math.h>
+#define _USE_MATH_DEFINES
 
 
 
@@ -23,7 +25,7 @@ int main() {
 
     // cv::Mat frame;
     // cap >> frame;
-    // cv::Mat frame = imread("assets/DEMO_circle_fish_star_01.jpg",cv::IMREAD_COLOR);
+    // cv::Mat frame = imread("./assets/DEMO_circle_fish_star_01.jpg",cv::IMREAD_COLOR);
     // cv::Mat frame_down;
     // cv::resize(frame,frame_down,cv::Size(),0.5,0.5);
     cv::Mat frame = imread("assets/DEMO_components_02.png",cv::IMREAD_COLOR);
@@ -40,50 +42,72 @@ int main() {
 
     cv::Mat bw;
 
-    cv::threshold(gray,bw,127,255,cv::THRESH_BINARY);
+    cv::threshold(gray,bw,190,255,cv::THRESH_BINARY_INV);
 
     cv::Mat temp;
     int numLables = cv::connectedComponents(bw,temp);
     cv::Mat output = cv::Mat::zeros(bw.size(),CV_8UC3);
+    int blob = 0;
 
-    for (int label = 1; label<numLables;++label){
-        cv::Mat mask = (temp != label);
+    std::vector<std::vector<cv::Point>> contours;
 
-        std::vector<std::vector<cv::Point>>contours;
-        cv::findContours(mask,contours,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_NONE);
-        cv::drawContours(output,contours,-1,cv::Scalar(255,0,0));
+    cv::findContours(bw,contours,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_NONE);
+
+    cv::drawContours(output,contours,-1,cv::Scalar(255,0,0));
+
+
+    for (auto contour : contours) {
+        blob++;
+
+        cv::Moments moments = cv::moments(contour);
+        double I = moments.m00;
+        std:: cout << "" << I<<"\n";
+        
+        double m10 = moments.m10;
+        double m01 = moments.m01;
+        double cx = m10 / I;
+        double cy = m01 / I;
+        double m11 = moments.m11;
+        std:: cout << "(" <<cx <<" , "<< cy <<")\n";
+
+        double ax = moments.m20;
+        double ay = moments.m02;
+        
+
+        double theta = 0.5 * std::atan2((2*((I*m11) - (m10 * m01))),(((I*ax)- std::pow(m10,2))-((I*ay)-std::pow(m01,2))));
+
+        std::cout << "Theta: "<< theta*(180/(std::atan(1)*4)) <<"";
+
+        cv::line(output, cv::Point(cx + 50, cy + theta*50), cv::Point(cx - 50, cy - theta * 50), (0,255,255), 2);
+
+        cv::line(output, cv::Point(cx + 25, cy), cv::Point(cx - 25, cy), (255,255,255), 2);
+        cv::line(output, cv::Point(cx, cy+25), cv::Point(cx, cy-25), (255,255,255), 2);
+
     }
-
+    
+    std::cout << ""<< blob<<"\n";
+    
     cv::imshow("original", bw);
     cv::imshow("contours", output);
 
-    int width = bw.size().width;
-    int height = bw.size().height;
+    // int width = bw.size().width;
+    // int height = bw.size().height;
 
-    std::cout << ""<< width << " x "<<height<<"\n";
+    // std::cout << ""<< width << " x "<<height<<"\n";
 
-    int count;
+    // int count;
 
-    for (int i = 0; i < width; ++i){
-        for (int j = 0; j <height; ++j){
-            cv::Vec3b pixel = bw.at<cv::Vec3b>(i,j);
-            if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0){
-                ++count;
-                if (count < 20){
-                    std::cout << "("<< i<<" , " << j << ")";
-                }
-            }
-        }
-    }
+    // for (int i = 0; i < width; ++i){
+    //     for (int j = 0; j <height; ++j){
+    //         cv::Vec3b pixel = bw.at<cv::Vec3b>(i,j);
+    //         if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0){
+    //             ++count;
+    //         }
+    //     }
+    // }
 
-    std::cout << ""<< count <<"";
-    /*
-    cv::Vec3b pixel = frame.at<cv::Vec3b>(y, x);
-    std::cout << "Pixel values at (" << x << ", " << y << "): ";
-    std::cout << "B: " << static_cast<int>(pixel[0]) << " ";
-    std::cout << "G: " << static_cast<int>(pixel[1]) << " ";
-    std::cout << "R: " << static_cast<int>(pixel[2]) << std::endl;
-    */
+    
+    // std::cout << ""<< count <<"";
     cv::waitKey(0);
     
     cap.release();
