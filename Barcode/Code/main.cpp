3b43acc891e20cc3de5b8bb129f2a259827fe1b3
Matt <mlee0108@student.monash.edu>
2024-04-12 14:47:06 +1000

    completed code using opencv lib

--- Barcode/Code/main.cpp
+++ Barcode/Code/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect/barcode.hpp>
 #include <iostream>
 #include <math.h>
 #define _USE_MATH_DEFINES
@@ -15,39 +16,73 @@ cv::Mat videocap(int fps){
 
     while (cap.read(frame)){
         
-        cv::Mat gray;
+        
 
-        cv::cvtColor(frame,gray,cv::COLOR_BGR2GRAY);
-
-        cv::imshow("webcam",gray);
+        cv::imshow("webcam",frame);
 
 
         if (cv::waitKey(1000/fps)>= 0){
             cv::destroyAllWindows();
             cap.release();
-            return gray;
+            return frame;
         }
     }
 
     return frame;
 }
-/*
+
 cv::Mat decoder(cv::Mat capture){
     if (capture.empty()){
         std::cout << "No capture given";
         return capture;
     }
+    cv::Mat gray;
 
-    cv::Mat corners;
+    cv::cvtColor(capture,gray,cv::COLOR_BGR2GRAY);
 
-}*/
+    std::vector<cv::Point> corners;
+    std::vector<std::string> decode_info;
+    std::vector<std::string> decode_type;
+
+    cv::Ptr ba = cv::makePtr<cv::barcode::BarcodeDetector>();
+   
+    ba -> detectAndDecodeWithType(gray,decode_info,decode_type,corners);
+
+    for (size_t i = 0; i < corners.size(); i += 4){
+        const size_t idx = i/4;
+        const bool isdec = idx < decode_info.size()
+        && idx < decode_type.size()
+        && !decode_type[idx].empty();
+        const cv::Scalar lineColor = isdec ? cv::Scalar(0,255,0) : cv::Scalar(255,0,0);
+        
+        std::vector<cv::Point> contour(corners.begin()+i,corners.begin() + i + 4);
+        const std::vector<std::vector<cv::Point>> contours{contour};
+        cv::drawContours(capture, contours,0,lineColor,1);
+
+        for (size_t j = 0; j < 4; j++){
+            cv::circle(capture,contour[j], 2, cv::Scalar(0,0,0),-1);
+        }
+
+        if (isdec){
+            std::ostringstream buf;
+            buf << "[" << decode_type[idx] << "]"<< decode_info[idx];
+            cv::putText(capture,buf.str(),contour[1],cv::FONT_HERSHEY_COMPLEX,0.8,cv::Scalar(0,0,255),2);
+        }
+    }
+    return capture;
+}
 
 
 int main(){
     cv::Mat capture;
 
     capture = videocap(20);
-    cv::imshow("capture",capture);
+
+    cv::Mat detect;
+
+    detect = decoder(capture);
+
+    cv::imshow("capture",detect);
     cv::waitKey(0);
     cv::destroyAllWindows();
     return 0;
