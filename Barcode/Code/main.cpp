ce92e4a4da0d6dbe80019993a2b0eff116dacdec
Audrey <atoo0005@student.monash.edu>
2024-04-12 11:40:35 +1000

    drawing contours of barcode

--- Barcode/Code/main.cpp
+++ Barcode/Code/main.cpp
@@ -4,30 +4,31 @@
 #include <math.h>
 #define _USE_MATH_DEFINES
 
-cv::Mat videocap(int fps){
+cv::Mat videocap(int fps) {
     cv::VideoCapture cap(0);
     cv::Mat frame;
 
-    if (!cap.isOpened()){
+    if (!cap.isOpened()) {
         std::cout << "Camera Cannot be Opened";
         return frame;
     }
 
-    while (cap.read(frame)){
-        
+    while (cap.read(frame)) {
+
         cv::Mat gray;
 
-        cv::cvtColor(frame,gray,cv::COLOR_BGR2GRAY);
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
-        cv::imshow("webcam",gray);
+        cv::imshow("webcam", gray);
 
 
-        if (cv::waitKey(1000/fps)>= 0){
+        if (cv::waitKey(1000 / fps) >= 0) {
             cv::destroyAllWindows();
             cap.release();
             return gray;
         }
     }
+
 
     return frame;
 }
@@ -43,11 +44,42 @@ cv::Mat decoder(cv::Mat capture){
 }*/
 
 
-int main(){
+cv::Mat draw_contours(cv::Mat gray) {
+
+
+    //Turns the image into a binary black and white photo
+    cv::Mat bw;
+    cv::threshold(gray, bw, 95, 255, cv::THRESH_BINARY_INV);
+
+    //Blurs the background to reduce unwanted blob detections
+    cv::Mat blured;
+    cv::GaussianBlur(bw, blured, cv::Size(5, 5), 0);
+
+    //creates a matrix to draw our contours on
+    cv::Mat temp;
+    int numLables = cv::connectedComponents(blured, temp);
+    cv::Mat output = cv::Mat::zeros(blured.size(), CV_8UC3);
+    int blob = 0;
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(blured, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+
+    //draws the contours onto the images
+    cv::drawContours(output, contours, -1, cv::Scalar(255, 0, 0));
+    return output;
+
+}
+
+
+int main() {
     cv::Mat capture;
 
     capture = videocap(20);
-    cv::imshow("capture",capture);
+    cv::imshow("capture", capture);
+
+    cv::Mat contours;
+    contours = draw_contours(capture);
+
+    cv::imshow("contours", contours);
     cv::waitKey(0);
     cv::destroyAllWindows();
     return 0;
